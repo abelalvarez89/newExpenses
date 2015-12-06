2d1bc98fcdfe02354fd1659da57e96fe5456afe1
Abel Alvarez <abelalvarez89@hotmail.com>
2015-12-06 00:41:31 -0500

    remove test directory with controller

--- src/test.controller.js
+++
@@ -1,4 +0,0 @@
-module.exports = function() {
-	var vm = this;
-	vm.stores = ['Publix', 'Walmart', 'House Rent', 'CVS', 'Midnight Cookies', 'Bestbuy', 'Taco Bell', 'McDonald', 'Subway', 'Shell', 'Aurora', 'Walgreens', 'Pizza Hut'];
-}
--- src/test.directive.js
+++
@@ -1,8 +0,0 @@
-module.exports = function() {
-	return {
-		restrict: 'EA',
-		template: require('./test.html'),
-		controller: require('./test.controller.js'),
-		controllerAs: 'vm'
-	}
-}
